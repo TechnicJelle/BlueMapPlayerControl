cd78d7fa14926a760abebb898e737e6211c1d5f1
TechnicJelle <22576047+TechnicJelle@users.noreply.github.com>
2022-08-31 18:41:40 +0200

    Finish up most important things

--- src/main/java/com/technicjelle/bluemapplayercontrol/BlueMapPlayerControl.java
+++ src/main/java/com/technicjelle/bluemapplayercontrol/BlueMapPlayerControl.java
@@ -1,25 +1,20 @@
 package com.technicjelle.bluemapplayercontrol;
 
 import com.technicjelle.bluemapplayercontrol.commands.BMPC;
-import de.bluecolored.bluemap.api.BlueMapAPI;
 import org.bukkit.Bukkit;
 import org.bukkit.command.PluginCommand;
 import org.bukkit.plugin.java.JavaPlugin;
 
 @SuppressWarnings("unused")
 public final class BlueMapPlayerControl extends JavaPlugin {
+	BMPC executor;
 
 	@Override
 	public void onEnable() {
-		// Plugin startup logic
 		getLogger().info("BlueMapPlayerControl enabled");
-		BlueMapAPI.onEnable(blueMapAPI -> {
-			getLogger().info("BlueMapAPI enabled");
-			getLogger().info("BlueMapAPI version: " + blueMapAPI.getAPIVersion());
-		});
 
 		PluginCommand bmpc = Bukkit.getPluginCommand("bmpc");
-		BMPC executor = new BMPC();
+		executor = new BMPC();
 		if(bmpc != null) {
 			bmpc.setExecutor(executor);
 			bmpc.setTabCompleter(executor);
@@ -31,7 +26,6 @@ public final class BlueMapPlayerControl extends JavaPlugin {
 
 	@Override
 	public void onDisable() {
-		// Plugin shutdown logic
 		getLogger().info("BlueMapPlayerControl disabled");
 	}
 }
--- src/main/java/com/technicjelle/bluemapplayercontrol/commands/BMPC.java
+++ src/main/java/com/technicjelle/bluemapplayercontrol/commands/BMPC.java
@@ -1,13 +1,15 @@
 package com.technicjelle.bluemapplayercontrol.commands;
 
 import de.bluecolored.bluemap.api.BlueMapAPI;
+import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.TabCompleter;
+import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
 
-import java.util.List;
+import java.util.*;
 
 public class BMPC implements CommandExecutor, TabCompleter {
 
@@ -19,15 +21,89 @@ public class BMPC implements CommandExecutor, TabCompleter {
 		if (BlueMapAPI.getInstance().isPresent()) {
 			BlueMapAPI api = BlueMapAPI.getInstance().get();
 
-			api.getWebApp().setPlayerVisibility(sender.getServer().getPlayerUniqueId(sender.getName()), false);
-			return true;
+			// === SELF ===
+			if(sender instanceof Player) { // only players can self
+				UUID senderUUID = sender.getServer().getPlayerUniqueId(sender.getName());
+				if (args.length == 0) {
+					//TODO: Toggle self
+					sender.sendMessage(ChatColor.YELLOW + "This command is not yet implemented. Please specify show/hide");
+					return true;
+				}
+				if (args.length == 1) {
+					if (args[0].equalsIgnoreCase("show")) {
+						api.getWebApp().setPlayerVisibility(senderUUID, true);
+						sender.sendMessage("You are now visible on the map");
+						return true;
+					} else if (args[0].equalsIgnoreCase("hide")) {
+						api.getWebApp().setPlayerVisibility(senderUUID, false);
+						sender.sendMessage("You are now invisible on the map");
+						return true;
+					}
+				}
+			}
+
+			// === OTHER ===
+			Player targetPlayer = sender.getServer().getPlayer(args[args.length - 1]); //if the last argument is a player name
+			if (targetPlayer == null) {
+				if(othersAllowed(sender)) {
+					sender.sendMessage(ChatColor.YELLOW + "Player not found");
+				} else {
+					noPermissionWarning(sender);
+				}
+				return true;
+			} else {
+				if(othersAllowed(sender)) {
+					UUID targetUUID = targetPlayer.getUniqueId();
+					if (args.length == 1) {
+						//TODO: Toggle other
+						sender.sendMessage(ChatColor.YELLOW + "This command is not yet implemented. Please specify show/hide");
+						return true;
+					} else if (args[0].equalsIgnoreCase("show")) {
+						api.getWebApp().setPlayerVisibility(targetUUID, true);
+						sender.sendMessage(targetPlayer.getDisplayName() + " is now visible on the map");
+						return true;
+					} else if (args[0].equalsIgnoreCase("hide")) {
+						api.getWebApp().setPlayerVisibility(targetUUID, false);
+						sender.sendMessage(targetPlayer.getDisplayName() + " is now invisible on the map");
+						return true;
+					}
+				} else {
+					noPermissionWarning(sender);
+					return true;
+				}
+			}
 		}
+
 		return false;
 	}
 
+	private void noPermissionWarning(CommandSender sender) {
+		sender.sendMessage(ChatColor.RED + "You are don't have permission to change the visibility of others");
+	}
 
 	@Override
 	public List<String> onTabComplete(CommandSender sender, Command command, String alias, String[] args) {
-		return null;
+		List<String> completions = new ArrayList<>();
+		if (args.length == 1) {
+			completions.add("show");
+			completions.add("hide");
+		}
+		if (othersAllowed(sender)) {
+			if (sender.getServer().getPlayer(args[0]) == null
+					|| args[0].equalsIgnoreCase("show")
+					|| args[0].equalsIgnoreCase("hide")
+					|| args[0].isBlank()) {
+				if(args.length <= 2) {
+					for (Player player : sender.getServer().getOnlinePlayers()) {
+						completions.add(player.getName());
+					}
+				}
+			}
+		}
+		return completions;
+	}
+
+	private boolean othersAllowed(CommandSender sender) {
+		return sender.isOp() || sender.hasPermission("bmpc.other");
 	}
 }
