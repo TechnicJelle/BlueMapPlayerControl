15a91141ee63199cb7cd625a6ed760a20f58c9de
TechnicJelle <22576047+TechnicJelle@users.noreply.github.com>
2023-05-04 04:47:34 +0200

    Update Checker and bStats

--- src/main/java/com/technicjelle/bluemapplayercontrol/BlueMapPlayerControl.java
+++ src/main/java/com/technicjelle/bluemapplayercontrol/BlueMapPlayerControl.java
@@ -1,17 +1,27 @@
 package com.technicjelle.bluemapplayercontrol;
 
+import com.technicjelle.UpdateChecker;
 import com.technicjelle.bluemapplayercontrol.commands.BMPC;
+import de.bluecolored.bluemap.api.BlueMapAPI;
+import org.bstats.bukkit.Metrics;
 import org.bukkit.Bukkit;
 import org.bukkit.command.PluginCommand;
 import org.bukkit.plugin.java.JavaPlugin;
 
-@SuppressWarnings("unused")
 public final class BlueMapPlayerControl extends JavaPlugin {
+	UpdateChecker updateChecker;
 	BMPC executor;
 
 	@Override
 	public void onEnable() {
 		getLogger().info("BlueMapPlayerControl enabled");
+
+		new Metrics(this, 18378);
+
+		updateChecker = new UpdateChecker("TechnicJelle", "BlueMapPlayerControl", getDescription().getVersion());
+		updateChecker.checkAsync();
+
+		BlueMapAPI.onEnable(api -> updateChecker.logUpdateMessage(getLogger()));
 
 		PluginCommand bmpc = Bukkit.getPluginCommand("bmpc");
 		executor = new BMPC();
@@ -21,7 +31,6 @@ public final class BlueMapPlayerControl extends JavaPlugin {
 		} else {
 			getLogger().warning("bmpc is null. This is not good");
 		}
-
 	}
 
 	@Override
